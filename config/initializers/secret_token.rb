8512d4b2a9fe66da7b06b83e987a131a875f98bb
Robert Teto <robertteto@Robert-Tetos-MacBook-Pro.local>
2011-07-25 23:20:48 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Daedalus::Application.config.secret_token = '155a44dc419ae228abfa16eeb2be22c7242e21cbcd81423dd078acaf00ff2b97abece9d795260f27576eb402ebb22155851a17108f7644f8d3e3e70b487d3314'
